38d1130d4175bf62253dc0e023e23a69843f3e5d
Federico <federico@fastmail.com>
2022-05-26 13:13:36 +0200

    Edited

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 
 
 def extract_text_from_pdf(pdf_file: str) -> [str]:
+    # Open the PDF file of your choice
     with open(pdf_file, 'rb') as pdf:
         reader = PyPDF2.PdfFileReader(pdf, strict=False)
         # no_pages = len(reader.pages)
@@ -10,14 +11,13 @@ def extract_text_from_pdf(pdf_file: str) -> [str]:
 
         for page in reader.pages:
             text = page.extract_text()
-
             pdf_text.append(text)
 
         return pdf_text
 
 
 def main():
-    extracted_text = extract_text_from_pdf('sample.pdf')
+    extracted_text = extract_text_from_pdf('YOUR_FILE.pdf')
     for t in extracted_text:
         # split_message = re.split(r'\s+|[,;?!.-]\s*', t.lower())
         print(t)
